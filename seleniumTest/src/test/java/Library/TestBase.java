9a6f9aa40a3e4fe66dc3ff065b5dd60c396d8f4d
Sharad.Chauhan <Sharad.Chauhan@C40FF82NCU.india.rsystems.com>
2018-12-10 17:56:02 +0530

    imp links

---
+++ seleniumTest/src/test/java/Library/TestBase.java
@@ -0,0 +1,68 @@
+package Library;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class TestBase {
+	
+	WebDriver driver;
+	
+	
+	
+	public void Openbrowser(String browser) {
+		
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sharad.Chauhan\\Desktop\\selenium\\chromedriver_win32\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+		
+	}
+	
+	 
+	public void capturescreenshot( String screenshotname) throws IOException {
+		// TODO Auto-generated method stub
+		
+		TakesScreenshot ts1 = (TakesScreenshot)driver;
+		
+		File src = ts1.getScreenshotAs(OutputType.FILE);
+		
+	   
+	    FileHandler.copy(src, new File("./Screenshots/"+screenshotname+ ".png"));
+	
+		
+	}
+public void Click(String xpathkey) {
+		
+		driver.findElement(By.xpath(xpathkey)).click();
+	}
+
+public void Navigate(String appurl) {
+	
+	driver.get(appurl);
+	
+	
+}
+
+public void sendkeys(String xpathkey2, String send) {
+	
+	driver.findElement(By.xpath(xpathkey2)).sendKeys(send);
+}
+
+public void quit() {
+	
+	driver.quit();}
+
+
+
+}
